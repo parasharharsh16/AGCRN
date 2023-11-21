6e415401799ee52ce21ecb34a12f95c4f08c0450
Harsh Parashar <parasharharsh16@gmail.com>
2023-11-21 10:22:24 +0530

    Changed dataset path

--- lib/load_dataset.py
+++ lib/load_dataset.py
@@ -4,10 +4,10 @@ import numpy as np
 def load_st_dataset(dataset):
     #output B, N, D
     if dataset == 'PEMSD4':
-        data_path = os.path.join('../data/PeMSD4/pems04.npz')
+        data_path = os.path.join('../data/PEMS04/PEMS04.npz')
         data = np.load(data_path)['data'][:, :, 0]  #onley the first dimension, traffic flow data
     elif dataset == 'PEMSD8':
-        data_path = os.path.join('../data/PeMSD8/pems08.npz')
+        data_path = os.path.join('../data/PEMS08/PEMS08.npz')
         data = np.load(data_path)['data'][:, :, 0]  #onley the first dimension, traffic flow data
     else:
         raise ValueError
